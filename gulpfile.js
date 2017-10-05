23e7f281ee68cec7b58c94746f56afecb016c15c
jreczko <reczko.jakub@gmail.com>
2017-10-05 21:54:27 +0200

    Improvements and clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 gulp.task('sass', function () {
     return gulp.src('./sass/**/*.scss')
-        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
 

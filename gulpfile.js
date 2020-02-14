d97b53148465a7bff6523e163670d9e98170463a
Jakub Reczko <contact@jakubreczko.com>
2020-02-14 07:41:31 +0100

    Remove compiled styles from source, move sources to the source directory

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,45 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const gulpDeployFtp = require('gulp-deploy-ftp');
+const ftpConfig = require('config').get('GentlemanBarber.ftpConfig');
 
-gulp.task('sass', function () {
+gulp.task('deploy', function () {
+    return gulp.src('./dist')
+        .pipe(gulpDeployFtp(ftpConfig));
+});
+
+gulp.task('styles', function () {
     return gulp.src('./src/sass/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./src/sass/**/*.scss', ['sass']);
+gulp.task('images', () => {
+    gulp.src('./src/img/*')
+        .pipe(image({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            guetzli: false,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true // defaults to false
+        }))
+        .pipe(gulp.dest('./dist/img'));
 });
+
+gulp.task('styles:watch', function () {
+    gulp.watch('./src/sass/**/*.scss', ['styles']);
+});
+
+gulp.task('images:watch', function () {
+    gulp.watch('./src/img/**/*.(png|jpg|gif)', ['images']);
+});
+
+gulp.task('watch', ['styles:watch', 'images:watch']);
+
+gulp.task('default', ['styles', 'images']);
